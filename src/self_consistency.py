ca5bff20a13f2200232ed24553d816311760698b
Aditya Parashar <adityaparashar95@gmail.com>
2024-03-21 02:36:55 -0400

    fixed demo path issue

--- src/self_consistency.py
+++ src/self_consistency.py
@@ -42,7 +42,8 @@ class SelfConsistency(LLMWrapper):
         self.default_metrics = default_metrics
         if args.dataset_name == 'strategy_qa':
             # extracting the few exemplars for strategy_qa
-            with('../data/demos/strategy_qa_demos.json', 'r') as f:
+            demo_file_path = '/work/pi_dhruveshpate_umass_edu/aparashar_umass_edu/Arithmetic-Sampling/data/demos/strategy_qa_demos.json'
+            with(demo_file_path, 'r') as f:
                 self.datasets['demo'] = json.load(f)
             filepath = "/work/pi_dhruveshpate_umass_edu/aparashar_umass_edu/Arithmetic-Sampling/data/stratqa_data.json"
             with open(filepath,'r') as f:
